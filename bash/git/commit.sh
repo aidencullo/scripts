3a2f525ac33b496820f500db666edc0358c2f39a
Aiden Cullo <aidencullo@gmail.com>
2024-01-29 17:22:19 -0500

    allow multiple sequential commits

--- bash/git/commit.sh
+++ bash/git/commit.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo "$(date)" >> file
+n=$1
+if [ $# -eq 0 ]; then
+    n = 1
+fi
 
-git ac "dummy commit"
+
+while [[ $x -lt $n ]]
+do
+    echo "$(date)" >> file
+    git ac "dummy commit"
+    x=$(( $x + 1 ))
+done
